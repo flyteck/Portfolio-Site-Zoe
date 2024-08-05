539557c57f193e5fb58e5f186fb3f7528b046f50
flyteck <naturegirl14@rogers.com>
2024-08-05 13:12:55 -0400

    Exercise 2.3
    > Added mobile hamburger menu w JS
    > Added jquery library
    > Small layout changes to nav

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log("weeeee I'm the console log!");
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
