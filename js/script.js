d4ef4d74cc517c34954f4a9ecc6501ec2d7e4266
flyteck <naturegirl14@rogers.com>
2024-08-06 09:57:01 -0400

    2.3 Updates
    Made the hamburger menu switch to an x on open, and back on close

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
-  
-  if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+  var navContainer = document.getElementById('myNavtoggle');
+  var navIcon = document.getElementById('navtoggle-icon');
+
+  if (navContainer.className === 'navtoggle') {
+    navContainer.className += ' responsive';
+    navIcon.classList.remove("fa-bars");
+    navIcon.classList.add("fa-xmark");
   } else {
-    x.className = 'navtoggle';
+    navContainer.className = 'navtoggle';
+    navIcon.classList.add("fa-bars");
+    navIcon.classList.remove("fa-xmark");
   }
 }
