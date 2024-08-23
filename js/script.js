1a5cc07b9567ff8a13de2e9fd2fd6cc7e2f17aa7
flyteck <naturegirl14@rogers.com>
2024-08-23 09:24:37 -0400

    Small Syntax/Format Fixes

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
 function menuToggle() {
-  var navContainer = document.getElementById('myNavtoggle');
-  var navIcon = document.getElementById('navtoggle-icon');
-
-  if (navContainer.className === 'navtoggle') {
-    navContainer.className += ' responsive';
-    navIcon.classList.remove("fa-bars");
-    navIcon.classList.add("fa-xmark");
-  } else {
-    navContainer.className = 'navtoggle';
-    navIcon.classList.add("fa-bars");
-    navIcon.classList.remove("fa-xmark");
-  }
+    var navContainer = document.getElementById("myNavtoggle");
+    var navIcon = document.getElementById("navtoggle-icon");
+    if (navContainer.className === "navtoggle") {
+        navContainer.className += " responsive";
+        navIcon.classList.remove("fa-bars");
+        navIcon.classList.add("fa-xmark");
+    } else {
+        navContainer.className = "navtoggle";
+        navIcon.classList.add("fa-bars");
+        navIcon.classList.remove("fa-xmark");
+    }
 }
