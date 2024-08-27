1091b0f5100180048938b5a41a2a5b430fe3592f
flyteck <naturegirl14@rogers.com>
2024-08-27 14:12:27 -0400

    Aquire Fixes
    > Jump links updated, mobile version styled
    
    > Images on Aquire page fixed

--- js/script.js
+++ js/script.js
@@ -9,3 +9,20 @@ function menuToggle() {
         navIcon.classList.remove("open");
     }
 }
+
+function jumpMenuOpen() {
+  var jumpContainer = document.getElementById("jump-menu");
+
+  if (jumpContainer.classList.contains("open")) {
+        jumpContainer.classList.remove("open");
+        jumpContainer.style.height = "";
+    } else {
+        jumpContainer.classList.add("open");
+
+        var containerHeight = jumpContainer.scrollHeight;
+        var containerHeight = containerHeight + "px";
+        //get the required height of the container, add "px" to it, and set the open container to that
+
+        jumpContainer.style.height = containerHeight;
+    }
+}
