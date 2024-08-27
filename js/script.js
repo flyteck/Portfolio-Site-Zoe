57cc718b1e9baad0b6e541e2bfbff89b5634f670
flyteck <naturegirl14@rogers.com>
2024-08-27 14:24:24 -0400

    +/x rotate for jump links
    small code error prevented it from being run

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
+//toggles the menu open/close for mobile
 function menuToggle() {
     var navContainer = document.getElementById("myNavtoggle");
     var navIcon = document.getElementById("navtoggle-icon");
+    //get the container, and hamburger icon
+
+    //if the menu is open, add open to the icon as well; else remove open from icon
     if (navContainer.className === "navtoggle") {
         navContainer.className += " responsive";
         navIcon.classList.add("open");
@@ -12,10 +16,14 @@ function menuToggle() {
 
 function jumpMenuOpen() {
   var jumpContainer = document.getElementById("jump-menu");
+  //get the jump menu container
 
+  //if container is open, remove open to close it
   if (jumpContainer.classList.contains("open")) {
         jumpContainer.classList.remove("open");
         jumpContainer.style.height = "";
+
+  //else, open it, and set the container's height to the content scrollheight to ensure smooth animation with dynamic height
     } else {
         jumpContainer.classList.add("open");
 
@@ -24,5 +32,10 @@ function jumpMenuOpen() {
         //get the required height of the container, add "px" to it, and set the open container to that
 
         jumpContainer.style.height = containerHeight;
+
+        //aaand as a fallback in case for some reason scrollheight returns null, set the container to auto so it still opens
+        if (containerHeight === null) {
+          jumpContainer.style.height = "auto";
+        }
     }
 }
