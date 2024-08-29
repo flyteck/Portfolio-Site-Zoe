1c40821af0c06cf577e6a7903337501d16f66be4
flyteck <naturegirl14@rogers.com>
2024-08-29 15:03:33 -0400

    Menu Collapse V2

--- js/script.js
+++ js/script.js
@@ -25,31 +25,43 @@ addEventListener("scroll", menuScroll);
 function menuScroll() {
     if (window.scrollY !== 0) {
       navContainer.classList.add("scrolled");
+      //set the position
+      absolutePosition();
     } 
     if (window.scrollY === 0) {
       navContainer.classList.remove("scrolled");
+      //after transitions are done, set the position
+      setTimeout(absolutePosition, 200);
     }
   }
-
 
 navContainer.addEventListener("mouseover", menuOpen);
 navContainer.addEventListener("mouseleave", menuClose);
 
-
-
 function menuOpen() {
   if (window.scrollY !== 0) {
     navContainer.classList.remove("scrolled");
+    //after transitions are done, set the position
+    setTimeout(absolutePosition, 200);
   } else {
     return
   }
-
 }
 
 function menuClose() {
   if (window.scrollY !== 0) {
     navContainer.classList.add("scrolled"); 
+    //set the position
+    absolutePosition();
   } else {
     return
+  }
+}
+
+function absolutePosition() {
+  if (navContainer.classList.contains("scrolled")) {
+    navContainer.style.position = "absolute";
+  } else {
+    navContainer.style.position = "relative";
   }
 }
